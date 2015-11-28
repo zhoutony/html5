f00ad7b36cfc935411b3fec71dbddff18aadfa7c
袁海雄 <yuanhaixiong@wepiao.com>
2015-11-28 18:50:37 +0800

    fix:server reversion

--- dev.js
+++ dev.js
@@ -18,7 +18,7 @@ process.on('uncaughtException', function(err) {
     console.error('Error caught in uncaughtException event:', err);
 });
 //git版本
-global.reversion = "a9d5000";
+global.reversion = "3202b75";
 
 global.staticBase = "";//"http://smart-static.wepiao.com";
 
--- server.js
+++ server.js
@@ -126,7 +126,7 @@ global.app				= express();
 global.logger 			= require("./log").logger("server");
 
 //git版本
-global.reversion = "a9d5000";
+global.reversion = "3202b75";
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
 
