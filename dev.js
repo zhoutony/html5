3e64be0d210c9761729ead22d4823a95366da543
袁海雄 <yuanhaixiong@wepiao.com>
2015-11-29 16:14:10 +0800

    fix:index news

--- dev.js
+++ dev.js
@@ -18,7 +18,7 @@ process.on('uncaughtException', function(err) {
     console.error('Error caught in uncaughtException event:', err);
 });
 //git版本
-global.reversion = "d632934";
+global.reversion = "a034736";
 
 global.staticBase = "";//"http://smart-static.wepiao.com";
 
--- server.js
+++ server.js
@@ -126,7 +126,7 @@ global.app				= express();
 global.logger 			= require("./log").logger("server");
 
 //git版本
-global.reversion = "d632934";
+global.reversion = "a034736";
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
 
