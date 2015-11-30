ff20cdeee6537816e3a182bbfde05198b7c6f6ff
Alanwang <alanwang@movikr.com>
2015-11-30 23:41:27 +0800

    文章展示页

--- dev.js
+++ dev.js
@@ -19,7 +19,7 @@ process.on('uncaughtException', function(err) {
 });
 //git版本
 
-global.reversion = "156606d";
+global.reversion = "c7be20d";
 
 
 global.staticBase = "";//"http://smart-static.wepiao.com";
--- server.js
+++ server.js
@@ -127,7 +127,7 @@ global.logger 			= require("./log").logger("server");
 
 //git版本
 
-global.reversion = "156606d";
+global.reversion = "c7be20d";
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
 
