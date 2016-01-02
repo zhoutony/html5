8ac8404751eef19e4de18c79e8b98c84f4e7a6ca
袁海雄 <yuanhaixiong@wepiao.com>
2016-01-02 20:10:45 +0800

    merge

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ global.app				= express();
 global.logger 			= require("./log").logger("server");
 
 //git版本
-global.reversion = "6675a60";
+global.reversion = "c2a92bd";
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
 
