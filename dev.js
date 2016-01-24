4d16ec224787f60c92a923bb1da975b2f7a92c2b
袁海雄 <yuanhaixiong@wepiao.com>
2016-01-24 20:56:11 +0800

    fix:css

--- dev.js
+++ dev.js
@@ -20,7 +20,7 @@ process.on('uncaughtException', function(err) {
 
 //git版本
 
-global.reversion = "81bd569";
+global.reversion = "a3b52f8";
 
 
 global.staticBase = "";//"http://smart-static.wepiao.com";
--- server.js
+++ server.js
@@ -127,7 +127,7 @@ global.logger 			= require("./log").logger("server");
 
 //git版本
 
-global.reversion = "81bd569";
+global.reversion = "a3b52f8";
 
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
