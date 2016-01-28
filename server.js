dbbd777e64140945c0a2eb4bab6669d1b856a74f
Alanwang <alanwang@movikr.com>
2016-01-28 10:46:57 +0800

    文章详情购票

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ global.logger 			= require("./log").logger("server");
 
 //git版本
 
-global.reversion = "a3b52f8";
+global.reversion = "b69ae4f";
 
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
