4486960c87ca212577002e47df264cd86ce8ad4d
袁海雄 <yuanhaixiong@wepiao.com>
2016-01-30 14:49:40 +0800

    fix: filmlist css

--- dev.js
+++ dev.js
@@ -20,7 +20,7 @@ process.on('uncaughtException', function(err) {
 
 //git版本
 
-global.reversion = "e192c46";
+global.reversion = "a575ec9";
 
 
 global.staticBase = "";//"http://smart-static.wepiao.com";
--- server.js
+++ server.js
@@ -127,7 +127,7 @@ global.logger 			= require("./log").logger("server");
 
 //git版本
 
-global.reversion = "e192c46";
+global.reversion = "a575ec9";
 
 
 global.staticBase = "http://js.moviefan.com.cn";//http://smart-static.wepiao.com/";
